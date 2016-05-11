a385d3f9e5a8965875bf82d0f44d225e4fb895b2
Emmanuel Odeke <emm.odeke@gmail.com>
2016-05-11 02:49:28 -0700

    cachy-server: allow GET, PUT, POST

--- cmd/cachy-server/main.go
+++ cmd/cachy-server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -36,17 +38,51 @@ type inRequest struct {
 	URI string `json:"uri"`
 }
 
-func Cache(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, fmt.Sprintf("%q not allowed", r.Method), http.StatusMethodNotAllowed)
-		return
+func parseInRequest(req *http.Request) (*inRequest, error) {
+	var body io.Reader
+	switch req.Method {
+	case "GET":
+		values := req.URL.Query()
+		kvList := []string{}
+		for key, _ := range values {
+			// TODO: Fix this hack since not only string values will be passed in
+			kvList = append(kvList, fmt.Sprintf("%q:%q", key, values.Get(key)))
+			fakeJSON := fmt.Sprintf("{%s}", strings.Join(kvList, ","))
+			body = strings.NewReader(fakeJSON)
+		}
+	case "POST", "PUT": // Lax method acceptance
+		limBody := io.LimitReader(req.Body, 1<<20)
+		body = limBody
+	default:
+		return nil, fmt.Errorf("Method %q not supported", req.Method)
 	}
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+	if body == nil {
+		return nil, fmt.Errorf("bug on: body is nil")
+	}
+
+	slurp, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+	inReq := &inRequest{}
+	if err := json.Unmarshal(slurp, inReq); err != nil {
+		return nil, err
+	}
+	return inReq, nil
+}
+
+func Cache(w http.ResponseWriter, r *http.Request) {
+	inReq, err := parseInRequest(r)
+	// Close the body first
+	_ = r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusMethodNotAllowed)
 		return
 	}
 
-	uri := strings.TrimSpace(r.PostForm.Get("uri"))
+	uri := strings.TrimSpace(inReq.URI)
 	if uri == "" {
 		http.Error(w, "'uri' expected in postForm", http.StatusBadRequest)
 		return
